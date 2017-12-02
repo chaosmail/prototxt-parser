a95a1f5b96757123a1498974722abeaf13a3b20d
Christoph Koerner <office@chaosmail.at>
2017-12-02 20:02:18 +0100

    add tinyify treeshaking

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 const browserify = require('browserify');
 const gulp = require('gulp');
 const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
 const rename = require('gulp-rename');
 const ts = require('gulp-typescript');
-const gutil = require('gulp-util');
 const runSequence = require('run-sequence');
 const del = require('del');
 const p = require('./package.json');
@@ -20,43 +15,44 @@ const config = {
 
 const tsProject = ts.createProject('tsconfig.json');
 
+function getBrowserify(tinyify) {
+  const b = browserify({
+    entries: config.src + '/index.ts',
+    standalone: 'prototxt-parser'
+  }).plugin('tsify');
+
+  return tinyify === true ? b.plugin('tinyify').bundle() : b.bundle();
+}
+
 // Clean the output folder
-gulp.task('clean', () => {
-  return del([config.dst]);
-});
+gulp.task('clean', () => del([config.dst]));
 
 // Bundle the project for the browser
 gulp.task('browserify', () => {
-  
-  let b = browserify({
-    standalone: 'prototxt-parser',
-    extensions: '.ts'
-  });
-
-  return b.plugin('tsify')
-    .add(config.src + '/index.ts')
-    .bundle()
+  return getBrowserify()
     .pipe(source(config.filename))
-    .pipe(buffer())
-    .pipe(gulp.dest(config.dst + '/browser'))
-    .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulpIf('*.js', rename({suffix: '.min'})))
+    .pipe(gulp.dest(config.dst + '/browser'));
+});
+
+// Bundle the project and tinify (flatPack, treeShake) it for the browser
+gulp.task('tinyify', () => {
+  return getBrowserify(true)
+    .pipe(source(config.filename))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(config.dst + '/browser'));
 });
 
 // Compile the project for Node and Typescript
 gulp.task('tsc', () => {
-
   return gulp.src([config.src + '/**/*.ts'])
     .pipe(tsProject())
     .pipe(gulp.dest(config.dst + '/node'));
 });
 
+// Build step: build JS, tiny JS and TS declaration in parallel
 gulp.task('build', (cb) => {
-  return runSequence('clean', ['browserify', 'tsc'], cb);
+  return runSequence('clean', ['browserify', 'tinyify', 'tsc'], cb);
 });
 
+// default task
 gulp.task('default', ['browserify']);
